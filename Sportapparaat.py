d301b9cd7c0af91b38d35e626a4e9c4f85f347a7
Suleyman54 <31888726+Suleyman54@users.noreply.github.com>
2018-01-30 15:04:12 +0100

    Add files via upload

--- Sportapparaat.py
+++ Sportapparaat.py
@@ -1,6 +1,25 @@
 from tkinter import *
 import tkinter
 import time
+import mysql.connector
+
+cnx = mysql.connector.connect(user='pi', password='l5VPO0VpD3eWvttbKYW4',
+                              host='46.101.127.123',
+                              database='sportschool')
+cursor = cnx.cursor()
+
+query = ("SELECT id FROM sport_type_names WHERE name = 'loopband'")
+
+
+cursor.execute(query)
+
+sport_id = []
+
+for v in cursor:
+    sport_id.append(v[0])
+
+
+
 
 class StopWatch(Frame):
     """ Implements a stop watch frame widget. """
@@ -39,14 +58,37 @@ class StopWatch(Frame):
             self._start = time.time() - self._elapsedtime
             self._update()
             self._running = 1
+            return time.time
+        return self._elapsedtime
 
     def Stop(self):
         """ Stop the stopwatch, ignore if stopped. """
         if self._running:
             self.after_cancel(self._timer)
             self._elapsedtime = time.time() - self._start
+            duration = float(self._elapsedtime)
+
+            afstand = float(duration * 2.777777778)
+            print("{0:.2f}".format(afstand))
+
             self._setTime(self._elapsedtime)
             self._running = 0
+
+            cursor_insert = cnx.cursor()
+
+            try:
+                query_insert = "INSERT INTO sport_activities (duration, distance, user_id, sport_equipment_id) VALUES (%s,%s,%s,%s)"
+                cursor_insert.execute(query_insert, (duration, ("{0:.2f}".format(afstand)), int(5), int(sport_id[0])))
+                cnx.commit()
+
+            except:
+                cnx.rollback()
+                print('lukt niet')
+
+            cnx.close()
+
+            return duration
+
 
     def Reset(self):
         """ Reset the stopwatch. """
@@ -56,64 +98,20 @@ class StopWatch(Frame):
         self._counter = 0
 
 
-
-
-    # def onClick(self):
-    #     self.counter = tkinter.IntVar()
-    #     self.counter.set(self.counter.get() + 1)
-        #
-        # # teller + 1
-        # def onClick(event=None):
-        #     counter.set(counter.get() + 1)
-        #
-        # # teller - 1
-        # def offClick(event=None):
-        #     counter.set(counter.get() - 1)
-
-
 def main():
     root = Tk()
     sw = StopWatch(root)
-    sw.pack(side=LEFT, expand=YES)
-    counter = tkinter.IntVar()
+    sw.pack(side=TOP)
 
-    # teller + 1
-    def onClick(event=None):
-        if counter.get() == 20:
-            return False
-        counter.set(counter.get() + 1)
-
-    # teller - 1
-    def offClick(event=None):
-       if counter.get() == 0:
-           return False
-       counter.set(counter.get() - 1)
-
-
-
-
-    c = tkinter.Label(root, textvariable=counter).pack(side=LEFT, expand=YES)
-    a = tkinter.Button(root, text="Decrease", command=offClick, fg="dark red", bg="white", state='normal').pack(side=BOTTOM, expand=YES)
-    b = tkinter.Button(root, text="Increase", command=onClick, fg="dark green", bg="white").pack(side=BOTTOM, expand=YES)
-
-    Button(root, text='Start', command=sw.Start).pack(side=LEFT, expand=YES, anchor=W, fill=X)
-    Button(root, text='Stop', command=sw.Stop).pack(side=LEFT, expand=YES, anchor=W, fill=X)
-    Button(root, text='Reset', command=sw.Reset).pack(side=LEFT, expand=YES, anchor=W, fill=X)
-    Button(root, text='Quit', command=root.quit).pack(side=LEFT, expand=YES, anchor=W, fill=X)
-    # Label(root, textvariable=sw.counter).pack(side=TOP)
-    # Button(root, text='Decrease', command=sw.onClick, fg="dark red", bg="white").pack(side=BOTTOM)
-    # Button(root, text='Increase', command=sw.onClick, fg="dark green", bg="white").pack(side=BOTTOM)
-    # counter = tkinter.IntVar()
-
-    # def onClick(event=None):
-    #     counter.set(counter.get() + 1)
-    #
-    # tkinter.Label(root, textvariable=counter).pack(side=RIGHT)
-    # tkinter.Button(root, text="Increase", command=onClick, fg="dark green", bg="white").pack(side=LEFT)
-
+    Button(root, text='Start', command=sw.Start).pack(side=LEFT)
+    Button(root, text='Stop', command=sw.Stop).pack(side=LEFT)
+    Button(root, text='Reset', command=sw.Reset).pack(side=LEFT)
+    Button(root, text='Quit', command=root.quit).pack(side=LEFT)
 
     root.mainloop()
 
 
 if __name__ == '__main__':
     main()
+cursor.close()
+cnx.close()
